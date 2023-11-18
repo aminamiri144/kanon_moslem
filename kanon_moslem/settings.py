23ca8ddaec4da04812d46a1c7f0bb5cba8598243
amiri <aminamiri144@gmail.com>
2023-11-18 12:00:20 +0330

    Add Templates Files

--- kanon_moslem/settings.py
+++ kanon_moslem/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = '_@9u$bl4vqbj%s10^h9sb=chzw6_0dym)g6m-%5s16v%s-reoj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'evaluation.apps.EvaluationConfig',
+    'members.apps.MembersConfig',
+    'inventory.apps.InventoryConfig',
+    'tuition.apps.TuitionConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'kanon_moslem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
+
+
+ASSETS_URL = '/assets/'
+ASSETS_ROOT =  os.path.join(BASE_DIR, 'templates/assets')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
